aaa5f40196bb900e7ea322c6b5392222cf85e657
Aneesh <aneeshsunganahalli@gmail.com>
2024-11-20 17:58:49 +0530

    Centered the project

--- airbnb-app/components/Card.jsx
+++ airbnb-app/components/Card.jsx
@@ -24,7 +24,20 @@ export default function Card() {
           <p>5.0<span className="number">(6)&#183;USA</span></p>
         </div>
         <p className="card-info">Life lessons with Katie Zaferes</p>
-        <p><bold className="bold">From $136</bold>/ person</p>
+        <p><span className="bold">From $136</span>/ person</p>
+      </div>
+      
+      <div className="cards">
+        <div className="card-image-container">
+          <img className="card-image" src="im12.png"/>
+          <div className="online">SOLD OUT</div>
+        </div>
+        <div className="rating">
+          <img className="star" src="Star 1.png"/>
+          <p>5.0<span className="number">(6)&#183;USA</span></p>
+        </div>
+        <p className="card-info">Life lessons with Katie Zaferes</p>
+        <p><span className="bold">From $136</span>/ person</p>
       </div>
       
     </div>
